1dc228b8318f35a8f6524347ee5001491a90b465
Paul Palmieri <paulpalmieri@outlook.fr>
2024-06-05 15:49:54 +0200

    handle multiple scenarios

--- examples/qa-automation/qa_automation.py
+++ examples/qa-automation/qa_automation.py
@@ -11,10 +11,59 @@ from lavague.core import WorldModel, ActionEngine
 from lavague.core.agents import WebAgent
 from lavague.drivers.selenium import SeleniumDriver
 
+TEMP_FILES_DIR = "./generated_files"
 
-def main(url, file_path):
-    # parse feature file
-    feature_name, feature_file_name, test_case = parse_feature_file(file_path)
+def main(url, feature_file):
+    print("--------------------------")
+    print("Generating tests for feature: ", feature_file)
+    print("--------------------------")
+    feature_name, feature_file_name, scenarios = parse_feature_file(feature_file)
+
+    for scenario in scenarios:
+        nodes, screenshot, selenium_code = run_test_case(url, scenario)
+        code = generate_test_code(url, feature_name, scenario, selenium_code, nodes, screenshot)
+        print(f"Test case processed successfully.")
+        # create the temporary directory if it doesn't exist
+        if not os.path.exists(TEMP_FILES_DIR):
+            os.makedirs(TEMP_FILES_DIR)
+
+        with open(f"{TEMP_FILES_DIR}/{feature_name}_{scenario['name']}.py", "w") as f:
+            f.write(code)
+           
+    print(f"LaVague has finished running the {len(scenarios)} scenarios for feature: {feature_file_name}")
+    print(f"Merging all LaVague generated code into a single file for feature: {feature_file_name}...")
+    
+    final_code = merge_files(feature_file)
+    
+    # write final code to file
+    with open(f"./tests/{feature_name}.py", "w") as f:
+        f.write(final_code)
+    
+    print(f"LaVague has finished generating test cases for feature: {feature_file_name}.")
+    print(f"File available in ./tests/{feature_name}.py ")
+
+def parse_feature_file(file_path):
+    # read the feature file
+    feature_file_name = os.path.basename(file_path)
+    feature_name = feature_file_name.split(".")[0]
+    with open(file_path, "r") as file:
+        file_contents = file.read()
+    
+    # split the feature file by test scenarios
+    scenarios = file_contents.split("Scenario:")
+    parsed_scenarios = []
+    for scenario in scenarios[1:]:
+        scenario_name, *scenario_steps = scenario.strip().split('\n')
+        parsed_scenarios.append({
+            "name": scenario_name.strip(),
+            "steps": [step.strip() for step in scenario_steps]
+        })
+
+    print("Parsed feature file: ", feature_file_name)
+    return feature_name, feature_file_name, parsed_scenarios
+
+def run_test_case(url, scenario):
+    test_case = "\n".join(scenario["steps"])
 
     # initialize the agent
     selenium_driver = SeleniumDriver(headless=False)
@@ -25,79 +74,45 @@ def main(url, file_path):
 
     # run the case case with the agent
     print("--------------------------")
-    print(f"Running test case:\n{test_case}")
+    print(f"Using LaVague to run test case:\n\n{test_case}\n")
+    
     agent.get(url)
     agent.run(objective)
 
     # perform RAG on final state of HTML page using the action engine
     print("--------------------------")
-    print(f"Processing run...\n{test_case}")
+    print(f"Processing test case...")
     nodes = action_engine.navigation_engine.get_nodes(
-        f"We have ran the test case, generate the final assert statement.\n\ntest case:\n{test_case}"
-    )
+            f"We have ran the test case, generate the final assert statement.\n\ntest case:\n{test_case}"
+        )
 
     # parse logs
     logs = agent.logger.return_pandas()
     last_screenshot_path = get_latest_screenshot_path(logs.iloc[-1]["screenshots_path"])
     b64_img = pil_image_to_base64(last_screenshot_path)
     selenium_code = "\n".join(logs["code"].dropna())
-
-    print("--------------------------")
-    print(f"Generating pytest code")
-
-    # generate test code
-    code = generate_pytest_code(
-        url, feature_file_name, test_case, selenium_code, nodes, b64_img
-    )
-    code = code.replace("```python", "").replace("```", "").replace("```\n", "").strip()
-
-    # write test code to file
-    with open(f"./tests/{feature_name}.py", "w") as f:
-        f.write(code)
-
-    print("--------------------------")
-    print(
-        f"Test code for feature: {feature_name} has been generated in tests/{feature_name}.py"
-    )
+    
+    agent.driver.driver.close()    
+    return nodes, b64_img, selenium_code
 
 
-def parse_feature_file(file_path):
-    feature_file_name = os.path.basename(file_path)
-    feature_name = feature_file_name.split(".")[0]
-    with open(file_path, "r") as file:
-        file_contents = file.read()
-    print("Parsed feature file: ", feature_file_name)
-
-    return feature_name, feature_file_name, file_contents
-
-
+# gets the latest screenshot taken by the agent
 def get_latest_screenshot_path(directory):
-    # List all files in the directory
     files = os.listdir(directory)
-
-    # Get the full path of the files
     full_paths = [os.path.join(directory, f) for f in files]
-
-    # Find the most recently modified file
     latest_file = max(full_paths, key=os.path.getmtime)
-
     return latest_file
 
-
+# converts a PIL image to base64
 def pil_image_to_base64(image_path):
-    # Open the image file
     with Image.open(image_path) as img:
-        # Convert image to BytesIO object
-        # img.show()
         buffered = BytesIO()
         img.save(buffered, format="PNG")
-        # Encode the BytesIO object to base64
         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
     return img_str
 
-
-# Generates pytest code using the OpenAI API
-def generate_pytest_code(url, feature_name, test_case, selenium_code, nodes, b64_img):
+# Generates pytest-bdd code for a single given test case
+def generate_test_code(url, feature_name, test_case, selenium_code, nodes, b64_img):
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     completion = client.chat.completions.create(
         model="gpt-4o",
@@ -120,9 +135,61 @@ def generate_pytest_code(url, feature_name, test_case, selenium_code, nodes, b64
             },
         ],
     )
-    return completion.choices[0].message.content
+    code = completion.choices[0].message.content
+    code = code.replace("```python", "").replace("```", "").replace("```\n", "").strip()
 
+    return code
 
+# Merges all individual test cases into a single code file for the feature being tested
+def merge_files(feature_file_path):
+    print("--------------------------")
+    print(f"Generating final code...")
+    print("--------------------------")
+
+    feature_file_content = ""
+    with open(feature_file_path, "r") as file:
+        feature_file_content = file.read()
+        
+    # print(f"Feature file content:\n{feature_file_content}")
+
+    # List all .py files in the directory
+    file_paths = [os.path.join(TEMP_FILES_DIR, file) for file in os.listdir(TEMP_FILES_DIR) if file.endswith('.py')]
+    base_prompt = f"Merge the following pytest-bdd files into a single file with unique steps for the following feature, only output python code and nothing else:\n\nfeature file:\n\n{feature_file_content}\n\n"
+
+    # Read the content of the code files
+    code_contents = []
+    for file_path in file_paths:
+        # print(f"Reading file: {file_path}")
+        with open(file_path, 'r') as file:
+            code_contents.append(file.read())
+
+    # Combine the prompt and code contents
+    combined_prompt = base_prompt + "\n\n"
+    for i, content in enumerate(code_contents):
+        combined_prompt += f"Code File {i+1}:\n{content}\n\n"
+       
+    # print(combined_prompt)
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": combined_prompt,
+                    },
+                ],
+            },
+        ],
+    )
+    code = completion.choices[0].message.content
+    code = code.replace("```python", "").replace("```", "").replace("```\n", "").strip()
+    
+    return code
+# Builds the prompt used to generate single test case code
 def build__prompt(url, feature_file_name, test_case, selenium_code, nodes):
     return f"""Generate a valid pytest-bdd file with the following inputs and examples to guide you:
 Base url:{url}\n
@@ -135,7 +202,7 @@ Examples:\n\n{EXAMPLES}
 
 
 SYSTEM_PROMPT = """
-You are an expert in software testing frameworks and Python code generation. You answer in python code only and nothing else. Your task is to:
+You are an expert in software testing frameworks and Python code generation. You answer in python markdown only and nothing else, don't include anything after the last backticks. Your task is to:
 
 1. Process a Gherkin test, existing Selenium code that was ran during the test by an agent, relevant HTML nodes, and the screenshot of the last state of the page.
 2. Generate an assert statement for the last condition of the test case.
@@ -227,14 +294,14 @@ if __name__ == "__main__":
         description="Process a URL and a file path to generate pytest-bdd test cases"
     )
     parser.add_argument(
-        "--url", type=str, required=True, help="The start URL for your test cases"
-    )
-    parser.add_argument(
-        "--feature-file",
+        "--feature",
         type=str,
         required=True,
         help="The path of the .feature file with your test cases",
     )
+    parser.add_argument(
+        "--url", type=str, required=True, help="The start URL for your test cases"
+    )
 
     args = parser.parse_args()
-    main(args.url, args.feature_file)
+    main(args.url, args.feature)
